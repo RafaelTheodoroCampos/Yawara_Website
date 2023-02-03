58e7b97d2aa2fc17e268cf18e559aac908e2e9a9
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2023-02-03 14:26:01 -0300

    feat: insert de roles no banco de dados

---
+++ src/main/resources/db/migration/V2__Insert_to_roles.sql
@@ -0,0 +1,5 @@
+INSERT INTO roles (id, name)
+VALUES ('a322478f-6fcd-4d35-9817-5704920baa34', 'ROLE_ADMIN');
+
+INSERT INTO roles (id, name)
+VALUES ('6dcd2b39-a348-4221-b0ee-58ba499c1e2c', 'ROLE_USER');
